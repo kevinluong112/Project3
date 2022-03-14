69025441dc02b87dd4cb07f8d07c9a04c88262c6
khai <diepvien00149@yahoo.com>
2022-03-13 23:12:56 -0500

    KhaiLuong

--- proj/views.py
+++ proj/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from ClientProfile.forms import ClientProfileForm
+from ClientProfile.models import ClientProfileModel
+
+
 
 # Create your views here.
 def homeview(request, *args, **kwargs):
@@ -8,4 +12,47 @@ def homeview(request, *args, **kwargs):
 
 def clienthome(request, *args, **kwargs):
     #return HttpResponse("<h1>Profile Page</h1>")
-    return render(request, "ClientProfile/clientmode.html", {})
+    return render(request, "clientmode.html", {})
+
+def profile(request, *args, **kwargs):
+    #return HttpResponse("<h1>Profile Page</h1>")
+    return render(request, "profile.html", {})
+
+# def fuelQuote(request, *args, **kwargs):
+#     #return HttpResponse("<h1>Fuel Quote</h1>")
+#     return render(request, "fuelquote.html", {})
+
+def fuelQuote(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = ClientProfileForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            price = form.cleaned_data['price']
+            history = form.cleaned_data['history']
+            summary = form.cleaned_data['summary']
+            new_client = ClientProfileModel(name, description, price, history, summary)
+            new_client.save()
+            return HttpResponseRedirect('/register/')
+    else:
+        form = ClientProfileForm
+    return render(request, "fuelquote.html", {'form':form})
+
+
+def history(request, *args, **kwargs):
+    #return HttpResponse("<h1>Fuel Quote History</h1>")
+    return render(request, "history.html", {})
+
+
+def login(request, *args, **kwargs):
+    #return HttpResponse("<h1>Login Page</h1>")
+    return render(request, "login.html", {})
+
+
+def register(request, *args, **kwargs):
+    return render(request, "register.html", {})
+
+def guestpage(request, *args, **kwargs):
+    return render(request, "guessmode.html", {})
+
+
